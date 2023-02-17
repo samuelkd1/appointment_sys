b3e7fe95ddca12bc0cf50041af30c36baf8a1f78
samuelkd1 <samuel.ofosuaah@outlook.com>
2023-02-17 23:54:03 +0000

    more adds

--- book_appointment.py
+++ book_appointment.py
@@ -5,6 +5,7 @@ Created on Sat Feb  4 20:07:51 2023
 
 @author: samkd
 """
+from datetime import datetime
 
 def book_appt():
     """
@@ -16,12 +17,23 @@ def book_appt():
     appointment slot 
     
     """
-    f_name = input("Please enter your first name:").upper()
-    l_name = input("Please enter your last name: ").upper()
-    date = input("Please enter date in yyyy-mm-dd format: ")
-    appointment_slot = [{"First name": f_name}, {"Last name": l_name}, {"Date of appointment": date}]
-                        
-    return appointment_slot
+    try:
+        f_name = input("Please enter your first name:").upper()
+        l_name = input("Please enter your last name: ").upper()
+        date = input("Please enter date of your choice in yyyy-mm-dd format: ")
+        appointment_slot = [{"First name": f_name}, {"Last name": l_name}, {"Date of appointment": date}]
+        appointment_slot[2]["Date of appointment"] = datetime.strptime(appointment_slot[2]["Date of appointment"], "%Y-%m-%d")
+        if appointment_slot["date of appointment"] < datetime.today():
+            print(""""Date has to be after today. Try again.""")
+        else:
+            print(appointment_slot)
+            return appointment_slot
+        
+    except:
+        print(""""Sorry you may have entered the date incorrectly.
+              \n Please try again, remember to use YYYY-MM-DD format""")
+              
+    
 
 
 book_appt()
