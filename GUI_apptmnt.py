e3e5c80546a7ed42f4e9205f70ae49fb337f1dc1
samuelkd1 <samuel.ofosuaah@outlook.com>
2023-02-18 00:20:42 +0000

    added radio buttons

--- GUI_apptmnt.py
+++ GUI_apptmnt.py
@@ -8,10 +8,20 @@ Created on Sat Feb  4 22:45:57 2023
 from tkinter import *
 
 root = Tk()
-root.title("The appointment booking system")
+# title of program
+root.title("The appointment booking system Admin")
 root.geometry("800x800")
 #root.eval('tk::PlaceWindow . center')
-Label_1 = Label(root, text = "Welcome to the appointment booking admin system\n Please choose your options", font = ("Futuristic", 25))
+Label_1 = Label(root, text = "Welcome to the appointment booking system\n Please choose your options", font = ("Futuristic", 25))
 Label_1.pack()
+
+# will make radio buttons
+
+Radiobutton(root, text = "Book appointment").pack()
+Radiobutton(root, text = "See booked appointments").pack()
+Radiobutton(root, text = "Edit appointments").pack()
+
+Button_click = Button(root, text = "select")
+Button_click.pack() 
 
 root.mainloop()
